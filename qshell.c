dcb53c97ec4bd2b872f3f3697fe316cf99a335a9
Chris Ponticello <christopher.ponticello@uqconnect.edu.au>
2013-08-20 17:25:27 +1000

    syncing files before system update. Better safe than sorry\!

--- qshell.c
+++ qshell.c
@@ -5,6 +5,7 @@
 #include "qshell.h"
 
 /* ------------------------------------------------------------------------- */
+/* Top level */
 
 int main (int argc, char * argv[]) {
 	init();
@@ -52,6 +53,9 @@ void parse_args (int argc, char * argv[]) {
 	}
 }
 
+/* ------------------------------------------------------------------------- */
+/*input parsing  */
+
 void read_input () {
 	/*
 	 * Main event loop to collect commands from input lines
@@ -61,37 +65,43 @@ void read_input () {
 	int charc, argc;
 
 	while (1) {
-		// 1. Print prompt
-		if (getcwd(strbuf,sizeof(strbuf))==NULL) {
-			fprintf(stderr, "getcwd() error : %s\n", 
-				strerror(errno));
-			exit(2);
+		/* 1. Print prompt (interactive mode) */
+		if (input==stdin) {
+			if (getcwd(strbuf,sizeof(strbuf))==NULL) {
+				fprintf(stderr, "getcwd() error : %s\n", 
+					strerror(errno));
+				exit(2);
+			}
+			fprintf(stdout, "[%s]? ",strbuf);
+			fflush(stdout);
 		}
-		fprintf(stdout, "[%s]? ",strbuf);
-		fflush(stdout);
 
-		// 2. Read from input stream. Max 128 characters.
-		//    Split on newline/EoF
+		/* 2. Read from input stream. Max 128 characters.
+		 *    Split on newline/EoF/comment(#)
+		 */
 		charc=0;
 		c='\0';
-		while ((c=fgetc(input))!='\n' && !feof(input) && (charc<128)) {
+		while ((c=fgetc(input))!='\n' && c!='#' && !feof(input) && \
+		       (charc<128)) {
 			strbuf[charc++]=c;
 		}
 		strbuf[charc]='\0';
-		//Line too long: flush characters until the next newline
-		if (charc==128) {
+		//flush characters until the next newline
+		if (charc==128 || c=='#') {
 			while (fgetc(input)!='\n' && !feof(input));
 		}
-		//File input: print the command line next to the prompt
+		#if DEBUG>0
 		if (input!=stdin) {
 			fprintf(stdout, "%s\n", strbuf);
 			fflush(stdout);
 		}
+		#endif
+		//warning for too many characters
 		if (charc==128) {
 			fprintf(stderr, WARN_CMDLINE_CHARS);
 		}
 
-		// 3. Separate arguments. Max 20 arguments.
+		/* 3. Separate arguments. Max 20 arguments. */
 		for (int i=0; i<20; i++) {
 			argv[i]=NULL;
 		}
@@ -106,28 +116,26 @@ void read_input () {
 					break;
 			}	
 		}
+		//warning for too many arguments
 		if (argc>20) {
 			argc=20;
 			fprintf(stderr, WARN_CMDLINE_ARGS);
 		}
 
-		// 4. Process input
-		//     - Ignore blank or comment lines
+		/* 4. Process input */
 		if (argc>0) {
-			if (argv[0][0]!='#') {
-				parse_input(argc, argv);
-			}
+			parse_input(argc, argv);
 		}
 
-		// 5. Print background process output/termination
-		//
-		//
-		//
+		/* 5. Print background process output/termination
+		 *
+		 */
 
-		// 6. STOP before we prompt again - is it EoF?
-		//     - For interactive mode, this is any zero-char read
-		//     - For file mode, we want to stop anytime we hit feof
-		if (((charc==0) && (c!='\n')) || \
+		/* 6. STOP before we prompt again - is it EoF?
+		 *     - For interactive mode, this is any zero-char read
+		 *     - For file mode, we want to stop anytime we hit feof
+		 */
+		if (((charc==0) && (c!='\n') && (c!='#')) || \
 		    (input!=stdin && feof(input))) {
 			stop();
 		}
@@ -139,12 +147,40 @@ void parse_input (int argc, char * argv[]) {
 	/*
 	 * Handle a list of arguments from an input line
 	 */
+	int pipePos=-1, inDirectPos=-1, outDirectPos=-1;
+	int comArgCounter=0;
+	bool valid=1;
+
 	#if DEBUG>1
 	fprintf(stderr, "argc=%d\n", argc);
 	for (int i=0; i<argc; i++) {
 		fprintf(stderr, "arg[%d]=%s\n", i, argv[i]);
 	}
 	#endif
+
+	// loop through the arguments
+	for (int i=0; i<argc; i++) {
+		// pipe (|)
+		if streq(argv[i], "|") {
+			comArgCounter=0;
+		}
+		// input redirect (<)
+		else if streq(argv[i], "<") {
+			comArgCounter=0;
+		}
+		// output redirect (>)
+		else if streq(argv[i], ">") {
+			comArgCounter=0;
+		}
+		// background (&)
+		else if streq(argv[i], "&") {
+			comArgCounter=0;
+		}
+		// other: command/arg
+		else {
+			comArgCounter++;
+		}
+	}
 }
 
 void load_input (char * fname) {
@@ -159,6 +195,87 @@ void load_input (char * fname) {
 	}
 }
 
+
+/* ------------------------------------------------------------------------- */
+/* process handling */
+
+void proc_parent_forked (int fdc, int fdv[], int argc, char *argv[], int pid) {
+        /*  
+         * >
+         */
+     
+}
+
+
+
+void proc_child_forked (int fdc, int fdv[], int argc, char *argv[], int pid) {
+        /*  
+         * >
+         */
+     
+}
+
+
+/* ------------------------------------------------------------------------- */
+/* signal handling */
+
+void sig_do_int () {
+	/*
+	 * >
+	 */
+	
+}
+
+void sig_do_child () {
+        /*
+         * >
+         */
+    
+}
+
+void sig_do_shutdown () {
+        /*
+         * >
+         */
+    
+}
+
+void sig_do_pipe () {
+        /*
+         * >
+         */
+    
+}
+
+void sig_setup () {
+        /*
+         * >
+         */
+    
+}
+
+void sig_cancel () {
+        /*
+         * >
+         */
+    
+}
+
+void sig_block () {
+        /*
+         * >
+         */
+    
+}
+
+void sig_unblock () {
+        /*
+         * >
+         */
+    
+}
+
+/* ------------------------------------------------------------------------- */
 
 void sys_message (int msgCode) {
 	/*
--- qshell.h
+++ qshell.h
@@ -21,11 +21,15 @@
 /* MACROS */
 
 #define BUFFER_INCREMENT 128
+
 #define USAGE "Usage: qshell [FILE]\n"
 #define WARN_CMDLINE_ARGS "Warning: command line input too long (>20 args)."\
 " Ignoring extra arguments\n"
 #define WARN_CMDLINE_CHARS "Warning: command line input too long"\
 " (>128 characters). Ignoring extra characters\n"
+
+#define streq(p,q) (strcmp(p,q)==0)
+
 #define DEBUG 2
 
 /* TYPES */
@@ -38,15 +42,30 @@ FILE * input;
 
 /* FUNCTION PROTOTYPES */
 
+/* top level */
 int main (int argc, char * argv[]);
-
+void parse_args (int argc, char * argv[]);
 void init ();
 void stop ();
-void parse_args (int argc, char * argv[]);
 
+/* input parsing */
 void read_input ();
 void parse_input (int argc, char * argv[]);
 void load_input (char * fname);
+
+/* signal handling */
+void sig_do_int ();
+void sig_do_child ();
+void sig_do_shutdown ();
+void sig_do_pipe ();
+void sig_setup ();
+void sig_cancel ();
+void sig_block ();
+void sig_unblock ();
+
+/* process handling */
+void proc_parent_forked (int fdc, int fdv[], int argc, char *argv[], int pid);
+void proc_child_forked (int fdc, int fdv[], int argc, char *argv[], int pid);
 
 void sys_message (int msgCode);
 /* */
